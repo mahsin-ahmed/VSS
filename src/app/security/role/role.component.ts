7fa51af342ce00438866ca004909417f4b8acbfe
mahsin-ahmed <mahsin.cse@gmail.com>
2023-11-09 01:02:47 +0600

    Role CRUD operation done

--- src/app/security/role/role.component.ts
+++ src/app/security/role/role.component.ts
@@ -1,10 +1,183 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { IndividualConfig } from 'ngx-toastr';
+import { AuthService } from 'src/app/auth/auth.service';
+import { CommonService, toastPayload } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-role',
   templateUrl: './role.component.html',
   styleUrls: ['./role.component.css']
 })
-export class RoleComponent {
+export class RoleComponent { 
+  isList:boolean=true;
+  isNew:boolean = true;
+  phone:string = '';  
+  toast!: toastPayload;
+
+  constructor(private cs:CommonService,
+    private httpClient: HttpClient,
+    public authService:AuthService) { 
+    this.get();
+  }
+
+  get(){
+    const oHttpHeaders = new HttpHeaders(
+    {
+        'Token':this.authService.UserInfo.Token
+    });
+    this.httpClient.get(this.authService.baseURL + '/api/Role?pi='+this.pageIndex+'&ps='+this.pageSize+'&phone='+this.phone,{headers: oHttpHeaders}).subscribe((res)=>{
+      if(res){
+        this.listRole = res;
+        
+      //#region paging
+      this.rowCount = this.listRole.length > 0 ? this.listRole[0].RowCount : 0;
+      this.totalRowsInList = this.listRole.length;
+      this.pager.totalPages = Math.ceil(this.rowCount / this.pageSize);
+      this.pager.pages = [];
+      for(var i = 0; i<this.pager.totalPages; i++){
+        this.pager.pages.push(i+1);
+      }
+      this.pageStart = (this.pageIndex * this.pageSize) + 1;
+      this.pageEnd = (this.pageStart - 1) + this.totalRowsInList;
+      //#endregion
+      }else{
+        this.showMessage('warning', 'Session expired, please login.');
+      }
+    });
+  }
+
+  validateForm():boolean{
+    var isValid:boolean=true;
+    if(this.Role.RoleName==undefined||this.Role.RoleName==null||this.Role.RoleName==''){
+      isValid = false;
+      this.showMessage('warning', 'Role name is required.');
+    }
+    return isValid;
+  }
+
+  add() {    
+    if(!this.validateForm()){
+      return;
+    }
+    const oHttpHeaders = new HttpHeaders(
+      {
+          'Token':this.authService.UserInfo.Token
+      });
+    //this.Role.CreateBy = this.authService.UserInfo.UserID;
+    this.httpClient.post(this.authService.baseURL + '/api/Role', this.Role,{headers: oHttpHeaders}).subscribe((res) => {
+      if (res == true) {
+        this.isList = true;
+        this.get();
+        this.reset();
+        this.showMessage('success', 'data added.');
+      } else {
+        this.showMessage('error', 'error occurred.');
+      }
+    });
+  }
+  edit(item:any){
+    this.Role ={
+      RoleId:item.RoleId,
+      RoleName:item.RoleName,
+      };
+      this.isList = false;
+  }
+  
+  update() {
+    if(!this.validateForm()){
+      return;
+    }
+    const oHttpHeaders = new HttpHeaders(
+      {
+          'Token':this.authService.UserInfo.Token
+      });
+    //this.Designation.UpdateBy = this.authService.UserInfo.UserID;
+    this.httpClient.put(this.authService.baseURL + '/api/Role', this.Role,{headers: oHttpHeaders}).subscribe((res)=>{
+      if(res == true){
+        this.isList = true;
+        this.get();
+        this.showMessage('success', 'data updated.');
+      }else{
+        this.showMessage('error', 'error occurred.');
+      }
+    });
+  }
+
+  remove(item:any){
+    const oHttpHeaders = new HttpHeaders(
+      {
+          'Token':this.authService.UserInfo.Token
+      });
+    this.httpClient.delete(this.authService.baseURL + '/api/Role/' + this.Role.RoleId,{headers: oHttpHeaders}).subscribe((res)=>{
+      if(res == true){
+        this.get();
+        this.showMessage('success', 'data removed.');
+      }else{
+        this.showMessage('error', 'error occurred.');
+      }
+    });    
+  }
+  
+  reset() {
+    this.Role ={
+    RoleId:0,
+    RoleName:'',
+    };
+  }
+  search(){};
+
+  listRole:any=[];
+  Role:{  
+    RoleId:number,
+    RoleName:string,
+  }={
+    RoleId:0,
+    RoleName:'',
+  };
+
+  //type: 'success', 'error', 'warning', 'info'
+  //message: '<span>Action in '+type+'</span>',
+  showMessage(type: string, message:string) {
+    this.toast = {
+      message:message,
+      title: type.toUpperCase(),
+      type: type,
+      ic: {
+        timeOut: 2500,
+        closeButton: true,
+      } as IndividualConfig,
+    };
+    this.cs.showToast(this.toast);
+  }
+
+
+  //#region paging varible
+  pageIndex: number = 0;
+  pageSize:number = 10;
+  rowCount:number = 0;
+  listPageSize:any = [5,10,20];
+  pageStart:number = 0;
+  pageEnd:number = 0;
+  totalRowsInList:number=0;
+  pagedItems:any = [];
+  pager:{
+    pages:any,
+    totalPages:number
+  } = {
+    pages:[],
+    totalPages:0
+  };  
+
+  changePageSize(){
+    this.pageIndex = 0;
+    this.get();
+  }
+
+  changePageNumber(pageIndex:number){
+    this.pageIndex = pageIndex;
+    this.get();
+  }
+  //#endregion
 
 }
