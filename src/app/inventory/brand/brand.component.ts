91789029e2b622abab776ec738c176ab530c39a2
mahsin-ahmed <mahsin.cse@gmail.com>
2023-11-08 10:32:41 +0600

    Designation Done

--- src/app/inventory/brand/brand.component.ts
+++ src/app/inventory/brand/brand.component.ts
@@ -30,6 +30,18 @@ export class BrandComponent {
     this.httpClient.get(this.authService.baseURL + '/api/Brand?pi='+this.pageIndex+'&ps='+this.pageSize+'&phone='+this.phone,{headers: oHttpHeaders}).subscribe((res)=>{
       if(res){
         this.listBrand = res;
+        
+      //#region paging
+      this.rowCount = this.listBrand.length > 0 ? this.listBrand[0].RowCount : 0;
+      this.totalRowsInList = this.listBrand.length;
+      this.pager.totalPages = Math.ceil(this.rowCount / this.pageSize);
+      this.pager.pages = [];
+      for(var i = 0; i<this.pager.totalPages; i++){
+        this.pager.pages.push(i+1);
+      }
+      this.pageStart = (this.pageIndex * this.pageSize) + 1;
+      this.pageEnd = (this.pageStart - 1) + this.totalRowsInList;
+      //#endregion
       }else{
         this.showMessage('warning', 'Session expired, please login.');
       }
